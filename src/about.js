8dc788c2e663b7dccfa9644bbf2262ef72b19970
SamuelC28 <samuel@noukod.com>
2019-09-25 18:44:45 -0400

    fixed untracked files

--- src/about.js
+++
@@ -1,7 +0,0 @@
-export const paragraph = document.createElement('p');
-paragraph.classList.add('tab__display', 'tab__display--about');
-paragraph.textContent = `La Maison Restaurant International Experience Haitian-inspired dishes with an international flair at La Maison, our famous Port-au-Prince restaurant. La Maison is open daily for breakfast, lunch and dinner.Open for breakfast, lunch and dinner. Dress code: Smart Casual.
-                         La Maison Restaurant International Experience Haitian-inspired dishes with an international flair at La Maison, our famous Port-au-Prince restaurant. La Maison is open daily for breakfast, lunch and dinner.Open for breakfast, lunch and dinner. Dress code: Smart Casual.
-                         La Maison Restaurant International Experience Haitian-inspired dishes with an international flair at La Maison, our famous Port-au-Prince restaurant. La Maison is open daily for breakfast, lunch and dinner.Open for breakfast, lunch and dinner. Dress code: Smart Casual.`;
-
-export default { paragraph };
